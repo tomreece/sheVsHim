afaabc2324c6d45dd09ab5579b8717e11d64d5e0
Frank <fmackey@indiana.edu>
2012-07-28 17:51:54 -0400

    I made change.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
   
   public static Result index() {
-    return ok(index.render("Your new application is ready. Tom rules. I take it back. Frank is actually an ok guy."));
+    return ok(index.render("Your new application is ready. Tom rules. I take it back. Frank is actually an ok guy.Change"));
   }
   
 }
